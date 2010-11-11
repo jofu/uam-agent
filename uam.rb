bd9cff56a7a65393e19efa91a64b8bc98fb2020b
Jonathan Furrer <jofu@jofu.com>
2010-11-11 09:34:53 -0800

    Added support for removefromgroup

--- uam.rb
+++ uam.rb
@@ -1,168 +1,88 @@
-module ExtendPuppetUser
-  def getPW
-    return nil unless self.ensure == :present
-    entScmd = open('|/usr/bin/getent shadow ' + self.name)
-    entSout = entScmd.read()
-    return entSout.split(/:/)[1]
-  end
-  def isLocked
-    return nil unless self.ensure == :present
-    pw = self.getPW
-    if pw =~ /^!!.*/
-      return true
+#!/usr/bin/env ruby
+
+# Client program for the mcollective package agent found at http://code.google.com/p/mcollective-plugins/
+#
+# Released under the GPLv2
+
+require 'mcollective'
+
+include MCollective::RPC
+
+
+
+options = rpcoptions do |parser, options|
+    options[:timeout] = 180
+
+    parser.define_head "Manage users"
+    parser.banner = "Usage: mc-uam [options] action user <passwd|group>"
+end
+
+#if MCollective::Util.empty_filter?(options[:filter])
+    #print("Do you really want to operate on services unfiltered? (y/n): ")
+    #STDOUT.flush
+
+    #exit unless STDIN.gets.chomp =~ /^y$/
+#end
+
+uam = rpcclient("uam", {:options => options})
+
+if ARGV.length == 2
+    action = ARGV.shift
+    user = ARGV.shift
+    unless action =~ /^(status|passwd|lock|remove|add|addtogroup|removefromgroup)$/
+        puts("Action has to be status, passwd, lock, remove, add or addgroup")
+        exit 1
+    end
+    if action =~ /^(addtogroup|passwd|addtogroup|removefromgroup)$/
+      puts "When using passwd, removefromgroup or addtogroup you must supply a 3rd argument with the password (encrypted) or group"
+      exit 1
+    end
+elsif ARGV.length == 3
+    action = ARGV.shift
+    user = ARGV.shift
+    unless action =~ /^(passwd|addtogroup|removefromgroup)$/
+        puts("Only the passwd, removefromgroup or addtogroup option takes a third argument")
+        exit 1
+    end
+    if action == "passwd"
+      passwd = ARGV.shift
+      group = ' '
     else
-      return false
+      group = ARGV.shift
+      passwd = ' '
+    end
+else
+    puts("Please specify a user and action")
+    exit 1
+end
+
+
+results = {}
+
+
+stats = uam.send(action, {:user => user, :passwd => passwd, :group => group}) do |resp|
+  
+  next if resp == nil
+  
+  value = resp[:body][:data][:output]
+  
+  results.include?(value) ? results[value] << resp[:senderid] : results[value] = [ resp[:senderid] ]
+  
+end
+
+puts "\rReport for '" + action + " " + user + "'\n" if results.size > 0
+puts "\rSummarized:\n\n" if results.size > 0
+results.keys.sort.each do |k|
+  printf("%-40s: %d occurences\n", k, results[k].size)
+end
+
+if options[:verbose]
+  puts "\nDetailed:\n\n"
+  results.keys.sort.each do |k|
+    results[k].sort.each do |host|
+      printf("%-40s: %s\n", host, k)
     end
   end
 end
 
-module MCollective
-  module Agent
-    class Uam<RPC::Agent
-      metadata  :name         => "SimpleRPC Agent For User Management",
-                :description  => "Agent To Manage Users",
-                :author       => "Jonathan Furrer",
-                :license      => "GPLv2",
-                :version      => "1.0",
-                :url          => "http://www.jofu.com/os/mcollective/agents",
-                :timeout      => 180
-
-      ["status", "lock", "add", "remove"].each do |act|
-        action act do
-          validate :user, :shellsafe
-          do_user_action(act.to_sym)
-        end
-      end
-      
-      action "passwd" do
-        validate :user, :shellsafe
-        validate :passwd, :shellsafe
-        require 'puppet'
-        user = ::Puppet::Type.type(:user).create(:name => request[:user]).provider
-        user.extend(ExtendPuppetUser)
-        if user.ensure == :absent
-          reply[:output] = "Fail: '" + request[:user] + "' does not exist"
-        else
-          user.password = request[:passwd]
-          if user.getPW == request[:passwd]
-            reply[:output] = "Success: '" + request[:user] + "' password changed"
-          else
-            reply[:output] = "Fail: '" + request[:user] + "' password NOT changed"
-          end
-        end
-      end
-      
-      action "addtogroup" do
-        validate :user, :shellsafe
-        validate :group, :shellsafe
-        require 'puppet'
-        user = ::Puppet::Type.type(:user).create(:name => request[:user]).provider
-        user.extend(ExtendPuppetUser)
-        group = ::Puppet::Type.type(:group).create(:name => request[:group]).provider
-        if group.ensure == :absent
-          reply[:output] = "Fail: '" + request[:group] + "' group does not exist"
-        else
-          if user.ensure == :absent
-            reply[:output] = "Fail: '" + request[:user] + "' does not exist"
-          else
-            if user.groups.split(/,/).include?(request[:group])
-              reply[:output] = "Fail: '" + request[:user] + "' is already in group '" + request[:group] + "'"
-            else
-              user.groups = user.groups + "," + request[:group]
-              if user.groups.split(/,/).include?(request[:group])
-                reply[:output] = "Success: '" + request[:user] + "' has been added to '" + request[:group] + "'"
-              else
-                reply[:output] = "Fail: '" + request[:user] + "' has NOT been added to '" + request[:group] + "'"
-              end
-            end
-          end
-        end
-      end
-      
-      action "removefromgroup" do
-        validate :user, :shellsafe
-        validate :group, :shellsafe
-        require 'puppet'
-        user = ::Puppet::Type.type(:user).create(:name => request[:user]).provider
-        user.extend(ExtendPuppetUser)
-        if user.ensure == :absent
-          reply[:output] = "Fail: '" + request[:user] + "' does not exist"
-        elseif user.groups.split(/,/).include?(request[:group])
-          reply[:output] = "Fail: '" + request[:user] + "' not in group '" + request[:group] +"'"
-        else
-          newgroups = nil
-          user.groups.split(/,/).each do |group|
-            next if group == request[:group]
-            newgroups == nil ? newgroups = group : newgroups = newgroups + ',' + group
-          end
-          user.groups = newgroups
-          reply[:output] = "Success: removed '" + request[:user] + "' from group '" + request[:group] + "'" unless user.groups.split(/,/).include?(request[:group])
-          reply[:output] = "Fail: '" + request[:user] + "' NOT removed from group '" + request[:group] + "'" if user.groups.split(/,/).include?(request[:group])
-        end
-        reply[:output] = "Fail: UNKNOWN ERROR" unless reply[:output]
-      end
-          
-        
-      
-      private
-      def do_user_action(action)
-        begin
-          require 'puppet'
-          user = ::Puppet::Type.type(:user).create(:name => request[:user]).provider
-          user.extend(ExtendPuppetUser)
-          
-          reply[:output] = "blah"
-          reply[:exitcode] = 0
-          
-          case action
-            when :lock
-              user.password = '!!' + user.getPW
-              if user.isLocked
-                reply[:output] = "Success: '" + request[:user] + "' is LOCKED"
-              else
-                reply[:output] = "Fail: '" + request[:user] + "' is NOT LOCKED"
-              end
-                
-            when :status
-              if user.ensure == :absent
-                reply[:output] = "User '" + request[:user] + "' not found"
-              else
-                reply[:output] = "User '" + request[:user] + "' exists"
-                reply[:output] = reply[:output] + " and is LOCKED" if user.isLocked
-              end
-              
-            when :remove
-              if user.ensure == :absent
-                reply[:output] = "Success: '" + request[:user] + "' already didn't exist"
-              else
-                user.delete
-                if user.ensure == :absent
-                  reply[:output] = "Success: '" + request[:user] + "' removed"
-                else
-                  reply[:output] = "Fail: '" + request[:user] + "' was not removed"
-                end
-              end
-              
-            when :add
-              if user.ensure == :present
-                reply[:output] = "Success: '" + request[:user] + "' already exists"
-              else
-                user.create
-                if user.ensure == :present
-                  reply[:output] = "Success: '" + request[:user] + "' added"
-                else
-                  reply[:output] = "Fail: '" + request[:user] + "' was not added"
-                end
-              end
-               
-            else
-              reply.fail "Unknown action #{action}"
-          end
-            
-        rescue Exception => e
-          reply.fail e.to_s
-        end
-      end
-    end
-  end
-end
+printrpcstats
